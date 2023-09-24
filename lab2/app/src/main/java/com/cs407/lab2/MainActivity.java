4177eb4866ed7ba3ca9695a5b902b5ac330bdaf6
dth2701 <dothienhuong2701@gmail.com>
2023-09-23 21:28:30 -0500

    Finish milestone 1

--- lab2/app/src/main/java/com/cs407/lab2/MainActivity.java
+++ lab2/app/src/main/java/com/cs407/lab2/MainActivity.java
@@ -1,10 +1,20 @@
 package com.cs407.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public void clickFunction(View view) {
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
 
+        Log.i("INFO", "Button Pressed");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
