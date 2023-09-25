6d7529e0cdb696c03a32c2c33e42f6a28a123532
dth2701 <dothienhuong2701@gmail.com>
2023-09-24 22:01:02 -0500

    Real Finish milestone 1!

---
+++ calculator/app/src/main/java/com/cs407/calculator/CalculatorActivity.java
@@ -0,0 +1,22 @@
+package com.cs407.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class CalculatorActivity extends AppCompatActivity {
+    TextView textView2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculator_activity);
+        textView2 = (TextView) findViewById(R.id.textView2);
+
+        Intent intent = getIntent();
+        double result =  intent.getDoubleExtra("result",0);
+        textView2.setText(String.valueOf(result));
+
+    }
+}
---
+++ lab2/app/src/main/java/com/cs407/lab2/CalculatorActivity.java
@@ -0,0 +1,20 @@
+package com.cs407.lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.content.Intent;
+
+public class CalculatorActivity extends AppCompatActivity {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculator_activity);
+        textView = (TextView) findViewById(R.id.textView);
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("message");
+        textView.setText("Hello "+str);
+    }
+}
